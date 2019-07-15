62c41b94d457df2fd0551625c2cfdebd527d6cc3
Bigshmow <dstewart88@gmail.com>
2019-07-15 10:42:56 -0700

    psuedo code written and added var/arr for game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,38 @@
 // alert("JS is linked!");
 
+/* Psuedo Code
+------------------
+Game Starts on load/refresh
+PC chooses random number to guess
+PC chooses random values for crystals
+    assigns those values
+User clicks crystals to play
+    every click adds to total score
+        if total score === pcguess
+            user wins++
+                reset to line 6
+        else
+            user losses++
+                reset to line 6
+
+*/
+
+// global variables
+// ------------------
+
+var pcguess;
+var crystalarr = [];
+var count = 0;
+var wins = 0;
+var losses = 0;
+
+// functions to call
+// ------------------
+
+
+
+// clickable functions
+// ------------------
 $("#crystal_1").click(function() {
     alert("Clickable")
 });
@@ -15,3 +48,6 @@ $("#crystal_3").click(function() {
 $("#crystal_4").click(function() {
     alert("Clickable")
 });
+
+// game process
+// ------------------
