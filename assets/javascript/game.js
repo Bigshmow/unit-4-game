29db8700df584faaf717abbf5ec454a3b8208c1b
Bigshmow <dstewart88@gmail.com>
2019-07-15 10:29:00 -0700

    adding clickable crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,17 @@
 // alert("JS is linked!");
+
+$("#crystal_1").click(function() {
+    alert("Clickable")
+});
+
+$("#crystal_2").click(function() {
+    alert("Clickable")
+});
+
+$("#crystal_3").click(function() {
+    alert("Clickable")
+});
+
+$("#crystal_4").click(function() {
+    alert("Clickable")
+});
